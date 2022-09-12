c6e7042e84c1b3610f1d134aba9563fef797b09d
jemin-kim <37904137+jemining@users.noreply.github.com>
2022-09-12 23:10:12 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.29 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,18 +1,15 @@
 function solution(s) {
-    let answer = [];
+    let answer = [0, 0];
     let strLen = 0;
-    let zeroCnt = 0;
-    let cnt = 0;
     
     while(s != "1"){
         let sTemp = s.split('0');
-        zeroCnt += sTemp.length - 1;
+        answer[1] += sTemp.length - 1;
         strLen = sTemp.join('').length;
 
         s = strLen.toString(2);
-        cnt++;
+        answer[0]++;
     }
 
-    answer = [cnt, zeroCnt];
     return answer;
 }
